eab750aa31130bf92292f88b128ee2e3e345bc6f
hyungpeal <peal0425@gmail.com>
2026-02-04 17:00:08 +0900

    fixed category feature, add document_shell_page feature

--- lib/ui/document/document_shell_page.dart
+++ lib/ui/document/document_shell_page.dart
@@ -4,6 +4,8 @@ import 'package:archivey/ui/document/view_model/doc_view_model.dart';
 import 'package:archivey/ui/document/widget/bottom_sheet_category_add_edit_widget.dart';
 import 'package:archivey/ui/document/widget/vertical_tab_navigation.dart';
 import 'package:archivey/utils/app_snack_bar_widget.dart';
+import 'package:archivey/utils/back_button_handler.dart';
+import 'package:archivey/utils/dismiss_keyboard.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
@@ -46,53 +48,57 @@ class DocumentShellPageState extends State<DocumentShellPage> {
     final currentLocation = GoRouterState.of(context).uri.toString();
     final bool isAllTotalDetailPage = currentLocation.contains('all_total/detail');
 
-    return Scaffold(
-      backgroundColor: appColorScheme.primary,
-      body: Row(
-        children: [
-          Expanded(
-            child: widget.contentPage,
+    return BackButtonHandler(
+      child: DismissKeyboard(
+        child: Scaffold(
+          backgroundColor: appColorScheme.primary,
+          body: Row(
+            children: [
+              Expanded(
+                child: widget.contentPage,
+              ),
+              if (!isAllTotalDetailPage)
+                Consumer<CategoryViewModel>(
+                    builder: (context, vm, _) {
+                      final displayCategories = [
+                        {'id': 'ALL', 'name': '전체'},
+                        ...vm.rootCategories.map((c) => {'id': c.categoryId, 'name': c.categoryName}),
+                      ];
+        
+                      return VerticalTabNavigation(
+                        categories: displayCategories.map((e) => e['name']!).toList(),
+                        selectedIndex: _getSelectedIndex(currentLocation, displayCategories),
+                        onTapChanged: (index) {
+                          final currentIdx = _getSelectedIndex(currentLocation, displayCategories);
+        
+                          if (currentIdx == index) {
+                            context.read<DocViewModel>().scrollToTop();
+                            return;
+                          }
+        
+                          if (index == -1) {
+                            setState(() => selectedIndex = -1);
+                            return;
+                          }
+        
+                          final selected = displayCategories[index];
+                          final String id = selected['id']!;
+                          final String name = selected['name']!;
+        
+                          if (selected['id'] == 'ALL') {
+                            context.go('/document_all_total');
+                          } else {
+                            /// ID를 경로에, 이름을 쿼리에 넣고 이동
+                            context.go('/document_category/$id?name=$name');
+                          }
+                        },
+                        isCategoryExist: vm.categories.isEmpty ? false : true,
+                      );
+                    }
+                ),
+            ],
           ),
-          if (!isAllTotalDetailPage)
-            Consumer<CategoryViewModel>(
-                builder: (context, vm, _) {
-                  final displayCategories = [
-                    {'id': 'ALL', 'name': '전체'},
-                    ...vm.rootCategories.map((c) => {'id': c.categoryId, 'name': c.categoryName}),
-                  ];
-
-                  return VerticalTabNavigation(
-                    categories: displayCategories.map((e) => e['name']!).toList(),
-                    selectedIndex: _getSelectedIndex(currentLocation, displayCategories),
-                    onTapChanged: (index) {
-                      final currentIdx = _getSelectedIndex(currentLocation, displayCategories);
-
-                      if (currentIdx == index) {
-                        context.read<DocViewModel>().scrollToTop();
-                        return;
-                      }
-
-                      if (index == -1) {
-                        setState(() => selectedIndex = -1);
-                        return;
-                      }
-
-                      final selected = displayCategories[index];
-                      final String id = selected['id']!;
-                      final String name = selected['name']!;
-
-                      if (selected['id'] == 'ALL') {
-                        context.go('/document_all_total');
-                      } else {
-                        /// ID를 경로에, 이름을 쿼리에 넣고 이동
-                        context.go('/document_category/$id?name=$name');
-                      }
-                    },
-                    isCategoryExist: vm.categories.isEmpty ? false : true,
-                  );
-                }
-            ),
-        ],
+        ),
       ),
     );
   }
